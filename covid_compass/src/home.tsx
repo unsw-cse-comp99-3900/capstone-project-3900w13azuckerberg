744d922d7e3bc7ff0fcddfd6c0c0b7003fd26b17
lachy <lachlan.ho@student.unsw.edu.au>
2024-06-22 17:38:06 +1000

    Revert "restructured to have separate compoenents"
    This reverts commit 43a7309c2141d3cf863835fd1d99a5a746535e9e.

--- covid_compass/src/home.tsx
+++ covid_compass/src/home.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet.heat';
 import 'leaflet/dist/leaflet.css';
-import './heatMap.css';
+import './home.css';
 
 
 const HeatMap: React.FC = () => {
+    const [showFilters, setShowFilters] = useState(false);
+
     useEffect(() => {
         const australiaBounds = L.latLngBounds(
             L.latLng(-45.2, 80),
@@ -52,8 +54,25 @@ const HeatMap: React.FC = () => {
         };
     }, []);
 
+    const toggleFilters = () => {
+        setShowFilters(!showFilters);
+    };
+
     return (
         <div id="map" style={{ height: '100vh', width: '100%' }}>
+            <div className="container">
+                <img src="logo.png" alt="logo.png" className="logo" />
+                <div className="filter-container">
+                    <i className="material-icons" onClick={toggleFilters}>menu</i>
+                    <div className={`filter-buttons ${showFilters ? 'show' : 'hide'}`}>
+                        <button className="filter-button">Alpha</button>
+                        <button className="filter-button">Beta</button>
+                        <button className="filter-button">Delta</button>
+                        <button className="filter-button">Omnicron</button>
+                        <button className="filter-button">Gamma</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
