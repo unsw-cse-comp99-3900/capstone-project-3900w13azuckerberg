a5a9448843b31cfe5502360aab6114d0f647a3ff
lachho <143239409+lachho@users.noreply.github.com>
2024-07-13 14:48:17 +1000

    Update init-db.sql to align with usernames

--- init-db.sql
+++ init-db.sql
@@ -1,5 +1,5 @@
 -- Create a new role
-CREATE ROLE user WITH LOGIN PASSWORD 'password' SUPERUSER CREATEDB CREATEROLE;
+CREATE ROLE user1 WITH LOGIN PASSWORD 'password' SUPERUSER CREATEDB CREATEROLE;
 
 -- Create a new database with user1 as the owner
 CREATE DATABASE covid OWNER user1;
