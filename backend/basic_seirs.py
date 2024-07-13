e244477ccc24d72f4c20c8a6bc67890a7398e776
Alan Ji <z5361814@ad.unsw.edu.au>
2024-07-13 14:04:03 +1000

    Working single model of infection

---
+++ backend/basic_seirs.py
@@ -0,0 +1,52 @@
+from seirsplus.models import *
+import networkx
+
+SIGMA = 1/5.2
+GAMMA = 1/10
+MU_I  = 0.002
+
+R0    = 2.5
+BETA  = 1/(1/GAMMA) * R0 
+
+model = SEIRSModel(initN   = 1000000,
+                   beta    = BETA, 
+                   sigma   = SIGMA, 
+                   gamma   = GAMMA, 
+                   mu_I    = MU_I,
+                   mu_0    = 0, 
+                   nu      = 0, 
+                   xi      = 0,
+                   beta_Q  = 0.5*(BETA), 
+                   sigma_Q = SIGMA, 
+                   gamma_Q = GAMMA, 
+                   mu_Q    = MU_I,
+                   theta_E = 0, 
+                   theta_I = 0, 
+                   psi_E   = 1.0, 
+                   psi_I   = 1.0,
+                   initI   = 10000, 
+                   initE   = 0, 
+                   initQ_E = 0, 
+                   initQ_I = 0, 
+                   initR   = 0, 
+                   initF   = 0)
+
+
+checkpoints = {'t':       [30, 90], 
+               'beta':    [BETA*0.5, BETA], 
+               'theta_E': [0.02, 0.02], 
+               'theta_I': [0.02, 0.02]
+              }
+
+model.run(T=300, checkpoints=checkpoints)
+
+for i in range(1,300, 10):
+    print("Total num infected at t =", i,"is", model.total_num_infected(t_idx=i) )
+
+
+model.figure_infections(vlines=checkpoints['t'], ylim=100000, plot_percentages=False)
+
+ref_model = SEIRSModel(beta=BETA, sigma=SIGMA, gamma=GAMMA, mu_I=MU_I, initI=10000, initN=1000000) 
+ref_model.run(T=300)
+
+model.figure_infections(vlines=checkpoints['t'], ylim=0.25, shaded_reference_results=ref_model)
--- backend/network_testing2.py
+++ backend/network_testing2.py
@@ -11,11 +11,14 @@ base_graph = nx.barabasi_albert_graph(n=num_nodes, m=3)
 # Define node attributes
 populations = np.random.randint(1000, 3000, num_nodes)
 initial_infections = np.random.randint(1, 100, num_nodes)
+
+print("InitI:", initial_infections)
+
 for i in range(num_nodes):
     base_graph.nodes[i]['population'] = populations[i]
     base_graph.nodes[i]['I'] = initial_infections[i]
-    base_graph.nodes[i]['E'] = 0
-    base_graph.nodes[i]['R'] = 0
+    base_graph.nodes[i]['E'] = 10
+    base_graph.nodes[i]['R'] = 20
 
 # Recalculate beta to ensure adequate transmission
 R0 = 2.5  # Basic reproduction number
@@ -28,10 +31,9 @@ model_params = {
     'sigma': 1/5.2,
     'gamma': gamma,
     'mu_I': 0.002,  # Adjusted mortality rate to a more realistic figure
-    'store_Xseries': True
+    'store_Xseries': True,
+    # 'initI': initial_infections
 }
-
-
 
 # Step 3: Initialize and run the SEIRS Model with verbose output
 model = SEIRSNetworkModel(G=base_graph, **model_params)
@@ -46,36 +48,35 @@ model.run(T=300, verbose=True)
 avg_degree = sum(dict(base_graph.degree()).values()) / num_nodes
 print(f"Average Degree: {avg_degree}")
 
-
-
-
-
 # Step 4: Extract and plot infection data
 infections_per_node = (model.Xseries == model.I).astype(int)
-node_id = 0
-plt.figure(figsize=(10, 5))
-plt.plot(model.tseries, infections_per_node[:, node_id], label=f'Infections in Suburb {node_id}')
-plt.title(f'Infection Spread Over Time in Suburb {node_id}')
-plt.xlabel('Time')
-plt.ylabel('Number of Infectious Individuals')
-plt.legend()
-plt.show()
+
+print("Infections_per_node", infections_per_node)
+
+# node_id = 5
+# plt.figure(figsize=(10, 5))
+# plt.plot(model.tseries, infections_per_node[:, node_id], label=f'Infections in Suburb {node_id}')
+# plt.title(f'Infection Spread Over Time in Suburb {node_id}')
+# plt.xlabel('Time')
+# plt.ylabel('Number of Infectious Individuals')
+# plt.legend()
+# plt.show()
 
 
-print("Initial infections:", initial_infections.sum())
-print("Initial susceptible:", (populations - initial_infections).sum())
+# print("Initial infections:", initial_infections.sum())
+# print("Initial susceptible:", (populations - initial_infections).sum())
 
-# After running the model
-print("Total infections over time:", model.numI)
+# # After running the model
+# print("Total infections over time:", model.numI)
 
-# Plotting more details
-plt.figure(figsize=(10, 5))
-plt.plot(model.tseries, model.numI, label='Infected')
-plt.plot(model.tseries, model.numS, label='Susceptible')
-plt.plot(model.tseries, model.numR, label='Recovered')
-plt.title('Detailed SEIRS Dynamics')
-plt.xlabel('Time')
-plt.ylabel('Number of Individuals')
-plt.legend()
-plt.show()
+# # Plotting more details
+# plt.figure(figsize=(10, 5))
+# plt.plot(model.tseries, model.numI, label='Infected')
+# plt.plot(model.tseries, model.numS, label='Susceptible')
+# plt.plot(model.tseries, model.numR, label='Recovered')
+# plt.title('Detailed SEIRS Dynamics')
+# plt.xlabel('Time')
+# plt.ylabel('Number of Individuals')
+# plt.legend()
+# plt.show()
 
