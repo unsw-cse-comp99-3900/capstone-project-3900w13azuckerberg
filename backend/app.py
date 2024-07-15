498a17530590725aa86222e14c5eb45f8e529a5d
Alan Ji <z5361814@ad.unsw.edu.au>
2024-07-15 16:20:20 +1000

    merged with db updates and tested

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ from db_manager import get_case_by_coordinate, get_case_by_loc, get_records, ini
 from model import db
 from gmaps import get_coordinates
 from datetime import datetime, timedelta
+from seirsplus.models import *
 import threading
 # from basic_seirs import get_predictive_data 
 
@@ -84,38 +85,99 @@ def heat_map():
     end_date = datetime.strptime('2023-12-30', '%Y-%m-%d').date()
 
     # Dictionary for daily cases grouped by location from 1-Jan-21 up until provided date
-    data = {}
+    map_data = {}
     
     # Loop over each day from start_date to end_date
     current_date = start_date
     while current_date <= end_date:
-        daily_cases = get_case_by_loc(current_date)
+        daily_cases = get_case_by_coordinate(current_date)
 
         # Initialize a list to store the cases for the current day
         cases_list = []
 
-        for location, intensity in daily_cases.items():
-            coordinates = get_coordinates(location)
+        for location, data in daily_cases.items():
+            # coordinates = get_coordinates(location)
             # print(coordinates)
 
-            if coordinates['latitude'] != 'Invalid location':
-                cases_list.append({
-                    "latitude": coordinates['latitude'],
-                    "longitude": coordinates['longitude'],
-                    "intensity": intensity
-                })
+            cases_list.append({
+                "latitude": data['latitude'],
+                "longitude": data['longitude'],
+                "intensity": data['case_count'],
+                "state": data["state"]
+            })
 
         # Use the date as a key in the data dictionary
-        data[current_date.strftime('%Y-%m-%d')] = cases_list
+        map_data[current_date.strftime('%Y-%m-%d')] = cases_list
         print("date:", current_date, "added")
         current_date += timedelta(days=1)
     print("number of days:", len(data))
-    return jsonify(data)
+    return jsonify(map_data)
 
 @app.route('/predictive_map', methods=['GET'])
 def predictive_map():
-    predictive_map = get_predictive_data()
-    return jsonify(predictive_map)
+    # predictive_map = get_predictive_data()
+
+    # dictionary to store map data
+    predictive_map_data = {}
+
+    # Initialise dictionary to store intial model parameters into
+    init_data = {}
+
+    # for each location in the db
+    # set beta, sigma, gamma
+    default_population = 10000
+    default_sigma = 1/5.2
+    default_gamma = 1/10
+    default_beta = 0.25
+    
+    predictive_period = 3 # one year of prediction
+
+    current_date = datetime.strptime('2024-4-30', '%Y-%m-%d').date()
+    loc_data = get_case_by_coordinate(current_date)
+
+    for key, data in loc_data.items():
+        init_data[key] = {
+            "latitude": data["latitude"],
+            "longitude": data["longitude"],
+            "state": data["state"],
+            "initN": default_population,
+            "initI": data["case_count"]
+        }
+
+    for location, data in init_data.items():
+        
+        # run model for each location
+        model = SEIRSModel(initN   = data["initN"],
+            beta    = default_beta, 
+            sigma   = default_sigma, 
+            gamma   = default_gamma, 
+            psi_E   = 1,
+            psi_I   = 1,
+            initI = data["initI"]
+            # initI   = 10000 
+        )
+
+        model.run(T = predictive_period, verbose=False)
+
+        for i in range(1, predictive_period):
+            date_key = (current_date + timedelta(days=i)).strftime('%Y-%m-%d')
+            
+            # need to initalise if date is not alr in dictionary
+            if date_key not in predictive_map_data:
+                predictive_map_data[date_key] = [] 
+            
+            # add predicted infection data to dictionary
+            predictive_map_data[date_key].append({
+                "lattitude": data["latitude"],
+                "longtitude": data["longitude"],
+                "state": data["state"],
+                "numS": round(model.numS[i*10]),
+                "numE": round(model.numE[i*10]),
+                "numI": round(model.numI[i*10]),
+                "numR": round(model.numR[i*10])
+            })
+
+    return jsonify(predictive_map_data)
 
 
 # variant filter for heatmap
@@ -164,9 +226,10 @@ def run_server():
     app.run(debug=True)
 
 if __name__ == '__main__':
-    # Start the Flask server in a separate thread
-    server_thread = threading.Thread(target=run_server)
-    server_thread.start()
+    # # Start the Flask server in a separate thread
+    # server_thread = threading.Thread(target=run_server)
+    # server_thread.start()
 
-    # Now call load_data() without blocking the main thread
-    load_data()
+    # # Now call load_data() without blocking the main thread
+    # load_data()
+    app.run(debug=True)
