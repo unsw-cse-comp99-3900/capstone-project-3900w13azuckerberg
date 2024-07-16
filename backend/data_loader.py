31024bf70cf77816a21c9c828a30950ee50dde65
Ethan Lin <eeeeethanl@gmail.com>
2024-07-16 22:54:14 +1000

    Added database component for strain labels

--- backend/data_loader.py
+++ backend/data_loader.py
@@ -4,18 +4,12 @@ from db_manager import load_dataframe_to_db
 def load_into_db(app):
     df = pd.read_parquet('raw_data/covid_data.parquet')
     lab_df = pd.read_parquet('raw_data/lab_location.parquet')
+    strain_df = pd.read_csv('raw_data/full_lineage_label.csv')
     load_dataframe_to_db(df, "virus_data", app)
     load_dataframe_to_db(lab_df, "lab_location", app)
+    load_dataframe_to_db(strain_df, "strain_label", app)
 
 if __name__ == '__main__':
     from app import app
     with app.app_context():
-        # df = clean_all_virus_data()
-        # lab_df = get_lab_df(df)
-        # df = clean_virus_data_after_lab(df, lab_df)
-
-        df = pd.read_parquet('raw_data/covid_data.parquet')
-        lab_df = pd.read_parquet('raw_data/lab_location.parquet')
-        # Load into DB
-        load_dataframe_to_db(df, "virus_data", app)
-        load_dataframe_to_db(lab_df, "lab_location", app)
+        load_into_db(app)
