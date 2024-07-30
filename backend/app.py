0356b0d216d31a2162f02289091cddb7486b5c6d
Alan Ji <z5361814@ad.unsw.edu.au>
2024-07-30 10:14:20 +1000

    historic seirs category data for bar chart

--- backend/app.py
+++ backend/app.py
@@ -275,6 +275,51 @@ def variant_pie_chart():
     print(f"Execution time: {execution_time} seconds")
     return jsonify(result_graph_data)
 
+@app.route('/SEIRS_data', methods=['GET'])
+def SEIR_data():
+    # infected_per_day = {
+    #     "2022-02-01": 423,
+    #     "2022-02-02": 431,
+    #     "2022-02-03": 389,
+    #     "2022-02-04": 237,
+    #     "2022-02-05": 531,
+    #     "2022-02-06": 543
+    # }
+
+    infected_per_day = get_all_case_by_date()
+
+
+    default_population = 100000000
+    default_sigma = 1/5.2
+    default_gamma = 1/10
+    default_beta = 0.25
+
+    SEIRS_output = {}
+
+    for day, infections in infected_per_day.items():
+
+        model = SEIRSModel(
+            initN   = default_population,
+            beta    = default_beta,
+            sigma   = default_sigma,
+            gamma   = default_gamma,
+            psi_E   = 1,
+            psi_I   = 1,
+            initI = infections
+        )
+
+        model.run(T = 2, verbose=False)
+
+        SEIRS_output[day] = {
+            "numS": round(model.numS[10]),
+            "numE": round(model.numE[10]),
+            "numI": round(model.numI[10]),
+            "numR": round(model.numR[10]),
+        }
+
+    return jsonify(SEIRS_output)
+
+
 
 def run_server():
     app.run(debug=True)
