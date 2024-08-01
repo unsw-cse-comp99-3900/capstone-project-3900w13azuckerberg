cae423ad3ac9bf6c6aeebf798919d6b9a7ae2ba9
Ethan Lin <eeeeethanl@gmail.com>
2024-08-01 17:34:44 +1000

    remove unused script

--- db/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "Initialization script started."
-
-echo "Running covid_backup.sql"
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/covid_backup.sql || {
-  echo "Failed to run covid_backup.sql"
-  exit 1
-}
