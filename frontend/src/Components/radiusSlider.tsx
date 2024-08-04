1af4d49aab0dca8dd567445ed6f9935be0cae89c
lachy <lachlan.ho@student.unsw.edu.au>
2024-08-04 18:48:56 +1000

    fixed spacing

--- frontend/src/Components/radiusSlider.tsx
+++ frontend/src/Components/radiusSlider.tsx
@@ -3,15 +3,15 @@ import { Slider } from '@mantine/core';
 import "./radiusSlider.css"
 
 interface RSliderProps {
-    setRadius: (token: number) => void;
-  }
+  setRadius: (token: number) => void;
+}
   
 const RadiusSlider: React.FC<RSliderProps> = ({ setRadius }) => (
-    <div id="container">
-        <Slider id="small" color='#0275ff' thumbSize={17} defaultValue={0}/>
-        <Slider id="slider" color='#0275ff'  thumbSize={20} defaultValue={20} onChange={setRadius} min={0} max={100} />
-        <Slider id="big" color='#0275ff' thumbSize={25} defaultValue={100}/>
-    </div>
+  <div id="container">
+    <Slider id="small" color='#0275ff' thumbSize={17} defaultValue={0}/>
+    <Slider id="slider" color='#0275ff'  thumbSize={20} defaultValue={20} onChange={setRadius} min={0} max={100} />
+    <Slider id="big" color='#0275ff' thumbSize={25} defaultValue={100}/>
+  </div>
 )
 
 export default RadiusSlider;
